1b1b9352cdb4ddc9e7d12dcbb74d51b571eacaa3
ThibautFgi <thibaut.fgi@hotmail.com>
2026-03-05 11:45:28 +0100

    test

---
+++ src/main/java/com/mountain_journey/mj_moteur/MjMoteurApplication.java
@@ -0,0 +1,13 @@
+package com.mountain_journey.mj_moteur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjMoteurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjMoteurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mountain_journey/mj_moteur/MjMoteurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountain_journey.mj_moteur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjMoteurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
