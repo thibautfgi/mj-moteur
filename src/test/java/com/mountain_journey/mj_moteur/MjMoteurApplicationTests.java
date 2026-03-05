bf793c5c8b22a9fd4bb2589dab015b12e88f805f
ThibautFgi <thibaut.fgi@hotmail.com>
2026-03-05 14:54:34 +0100

    Fix/test

--- src/test/java/com/mountain_journey/mj_moteur/MjMoteurApplicationTests.java
+++ src/test/java/com/mountain_journey/mj_moteur/MjMoteurApplicationTests.java
@@ -2,12 +2,15 @@ package com.mountain_journey.mj_moteur;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MjMoteurApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Vérifie que le contexte Spring démarre sans erreur
 	}
 
 }
